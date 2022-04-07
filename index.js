718277224e3dc4d7be027699d40469fe64b05e0b
Catherine_Vuthi <vuthi.esther@gmail.com>
2022-04-07 22:34:58 +0300

    function continuation lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(action = "go to the office") {
+    return `This Monday, I will ${action}.`;
+}
+
+function wrapAdjective(str = 'a dedecated programmer') {
+    return function (par = "a hard worker") {
+        return `You are ${par}!`;
+    }
+}
